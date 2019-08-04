54caccc060450fa14cfc1d35cba6c57285f1322e
Alper Konuralp <alper@konuralp.gen.tr>
2019-08-04 13:51:27 +0300

    Initial Commit

---
+++ src/main/java/com/dteknoloji/game/App.java
@@ -0,0 +1,13 @@
+package com.dteknoloji.game;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/dteknoloji/game/AppTest.java
@@ -0,0 +1,20 @@
+package com.dteknoloji.game;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
