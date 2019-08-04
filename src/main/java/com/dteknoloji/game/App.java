27cdf3b9df83ea25775c97d199c043f8f69a2369
Alper Konuralp <alper@konuralp.gen.tr>
2019-08-04 13:56:32 +0300

    First Game Version

--- src/main/java/com/dteknoloji/game/App.java
+++ src/main/java/com/dteknoloji/game/App.java
@@ -1,13 +1,33 @@
 package com.dteknoloji.game;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Sayı Bulma Oyunu");
+        int tutulanSayi = (int) Math.round(Math.random() * 1000);
+        boolean bildi = false;
+        Scanner scanner = new Scanner(System.in);
+        for (int sans = 1; sans <= 10; sans++) {
+            System.out.print(sans + ". Şansınız: ");
+            int cevap = scanner.nextInt();
+            if (cevap == tutulanSayi) {
+                System.out.println("Cevabı bildiniz. Tebrikler");
+                bildi = true;
+                break;
+            }
+            if (cevap > tutulanSayi) {
+                System.out.println("Cevabınızı küçültmeniz lazım!");
+            } else if (cevap < tutulanSayi) {
+                System.out.println("Cevabınızı büyütmeniz lazım!");
+            }
+        }
+        if (!bildi) {
+            System.out.printf("Maalesef bilemediniz. Cevap : %d olacaktı.\n", tutulanSayi);
+        }
     }
 }
